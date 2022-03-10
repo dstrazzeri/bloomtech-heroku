e3e28f1ddc146909e2194dbf3dc4bbaf15ed13df
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2022-03-10 12:58:20 -0500

    env.port

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+require('dotenv').config();
 
+const PORT = process.env.PORT || 9000;
+
+const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1>')
 })
-server.listen(9000, () => {
-    console.log('server is up on port 9000!')
+
+server.listen(PORT, () => {
+    console.log(process.env.PORT);
+    console.log(`server is up on port ${PORT}, user is ${process.env.username}`)
 });
